c2e31ce7f034949f810952e15a1dac7e9e672679
SergioSevaRayos <sergiosevarayos@gmail.com>
2023-11-03 20:34:46 +0100

    V0.0 Creación del repositorio

---
+++ src/sistemaPago/Principal.java
@@ -0,0 +1,14 @@
+package sistemaPago;
+
+import java.util.*;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		/* Variables */
+		Scanner sc = new Scanner(System.in); // Variable para crear el escaner
+		
+
+	}
+
+}
