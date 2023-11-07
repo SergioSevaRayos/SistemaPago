0ebfb7dde4a71d964625fe13e2b9209093ddbdce
SergioSevaRayos <sergiosevarayos@gmail.com>
2023-11-07 22:43:41 +0100

    V0.0 Cambios en como se muestran los datos

--- src/sistemaPago/Principal.java
+++ src/sistemaPago/Principal.java
@@ -97,8 +97,7 @@ public class Principal {
 		private static void efectivo() {
 
 			while (true) {
-				System.out.println("Efectivo");
-	            System.out.print("Introduce la cantidad a pagar\nCantidad: ");
+				System.out.print("Efectivo\nIntroduce la cantidad a pagar\nCantidad: ");
 	            inputPagar = sc.nextLine();
 	            System.out.print("Ahora introduce el efectivo\nEfectivo: ");
 	            String inputEfectivo = sc.nextLine();
@@ -113,22 +112,25 @@ public class Principal {
 	        }
 
 			 double[] valores = {500,200,100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01};
-			 	System.out.println("Devolución: " + cantidadADevolver + "\n");
-		        System.out.println("La devolución con la cantidad mínima de billetes y monedas es:\n");
-
+			 	System.out.println("-----------------------------------------------------"
+			 			+ "\nRESUMEN"
+			 			+ "\nDevolución: " + cantidadADevolver + " €"
+			 			+ "\nLa cantidad mínima de billetes y monedas es:");
 		        for (double valor : valores) {
 		            int cantidad = (int)(cantidadADevolver / valor);
 		            if (cantidad > 0) {
 		                System.out.println(cantidad + " " + (valor >= 1 ? "billetes" : "monedas") + " de " + valor + " euros");
 		                cantidadADevolver -= cantidad * valor;
-		            }  
+		            }
 		        }
-			
+		        System.out.println("-----------------------------------------------------");
+	            System.out.println("");
 		}
 
 		private static void tarjeta() {
-			System.out.println("Forma de pago mediante tarjeta seleccionado\n");
-			System.out.println("Formatos de numeración de las tarjetas\n"
+			System.out.println("");
+			System.out.println("Forma de pago mediante tarjeta seleccionado"
+							 + "\nFormatos de numeración de las tarjetas\n"
 							 + "0000 0000 0000 0000 --> Visa\n"
 							 + "0000 0000 0000 0000 --> MasterCard\n"
 							 + "0000 000000 00000   --> American Expres\n");
@@ -140,13 +142,13 @@ public class Principal {
 			int visaOk		= entradaTarj.indexOf("4");
 			int masterOk 	= entradaTarj.indexOf("5");
 			
-			String formatoVisMasOk = "\\d{4} \\d{4} \\d{4} \\d{4}";
-			String formatoAmericanOk = "\\d{4} \\d{6} \\d{5}";
+			String formatoVisMasOk 		= "\\d{4} \\d{4} \\d{4} \\d{4}";
+			String formatoAmericanOk 	= "\\d{4} \\d{6} \\d{5}";
 
 
 			if (americanOk == 0) {
-				System.out.println("Tipo 	--> American Express");
-				System.out.println("Número 	--> " + entradaTarj);
+				System.out.println("Tipo 	--> American Express\n"
+						+ "Número 	--> " + entradaTarj);
 				americanExpress = true;
 				if (entradaTarj.matches(formatoVisMasOk)) {
 					System.out.println("Formato adecuado");
@@ -166,8 +168,8 @@ public class Principal {
 				}
 
 			}else if (visaOk == 0) {
-				System.out.println("Tipo 	--> Visa");
-				System.out.println("Número 	--> " + entradaTarj);
+				System.out.println("Tipo 	--> Visa\n"
+						+ "Número 	--> " + entradaTarj);
 				visa = true;
 				if (entradaTarj.matches(formatoVisMasOk)) {
 					System.out.println("Formato adecuado");
@@ -186,8 +188,8 @@ public class Principal {
 					System.err.println("ERROR: Formato incorrecto");
 				}
 			}else if (masterOk == 0) {
-				System.out.println("Tipo 	--> Master Card");
-				System.out.println("Número 	--> " + entradaTarj);
+				System.out.println("Tipo 	--> Master Card\n"
+						+ "Número 	--> " + entradaTarj);
 				masterCard = true;
 				if (entradaTarj.matches(formatoAmericanOk)) {
 					System.out.println("Formato adecuado");
@@ -217,12 +219,12 @@ public class Principal {
 			}else if (masterCard) {
 				vis = "MasterCard";
 			}
-			System.out.println("\n-------------------------------------------------");
-			System.out.println("Resumen de la operación");
-			System.out.println("Tipo --> " + vis);
-			System.out.println("Cantidad a abonar: " + inputPagar + " €");
-			System.out.println("Número de tarjeta: " + entradaTarj + "");
-			System.out.println("Estado --> PAGO REALIZADO");
-			System.out.println("-------------------------------------------------");
+			System.out.println("\n-------------------------------------------------\n"
+					+ "Resumen de la operación\n"
+					+ "Tipo --> " + vis + "\n"
+					+ "Cantidad a abonar: "  + inputPagar + " €" + "\n"
+					+ "Número de tarjeta: " + entradaTarj + "\n"
+					+ "Estado --> PAGO REALIZADO\n"
+					+ "-------------------------------------------------");
 		}
 }
