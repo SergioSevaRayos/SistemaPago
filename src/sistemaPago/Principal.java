60ce5128e9dac91c6709fd6b71dea1ccedff52a4
SergioSevaRayos <sergiosevarayos@gmail.com>
2023-11-07 22:25:41 +0100

    V0.0 Implementados los métodos de pago con efectivo y tarjeta

--- src/sistemaPago/Principal.java
+++ src/sistemaPago/Principal.java
@@ -3,32 +3,32 @@ package sistemaPago;
 import java.util.*;
 
 /*
-Crea un pequeño programa que sirva para validar el pago dentro de un restaurante. Un cliente puede pagar en efectivo o con tarjeta. 
+Crea un pequeÃ±o programa que sirva para validar el pago dentro de un restaurante. Un cliente puede pagar en efectivo o con tarjeta. 
 El programa debe hacer los siguiente en cada caso:
 
 Preguntar el tipo de pago.
 
-Si se paga en efectivo, comprobar si la cantidad es exacta, en caso contrario devolver el mínimo dinero posible.
+Si se paga en efectivo, comprobar si la cantidad es exacta, en caso contrario devolver el mÃ­nimo dinero posible.
 
-ANOTACIÓN
+ANOTACIÃ“N
 
-Si seleccionamos la forma de pago en efectivo, el programa nos pedirá que introduzcamos la cantidad que vamos a entregar para realizar el pago. 
-El programa nos devolverá el dinero sobrante en billetes de 50€, 20€, 10€ y 5€, y también devolverá monedas de euro y céntimos. Las cantidades 
-devueltas será en base a la premisa de devolver la menor cantidad de billetes posibles, es decir, debe devolver siempre todos los billetes de 
+Si seleccionamos la forma de pago en efectivo, el programa nos pedirÃ¡ que introduzcamos la cantidad que vamos a entregar para realizar el pago. 
+El programa nos devolverÃ¡ el dinero sobrante en billetes de 50â‚¬, 20â‚¬, 10â‚¬ y 5â‚¬, y tambiÃ©n devolverÃ¡ monedas de euro y cÃ©ntimos. Las cantidades 
+devueltas serÃ¡ en base a la premisa de devolver la menor cantidad de billetes posibles, es decir, debe devolver siempre todos los billetes de 
 mayor importe que se pueda.
-Por ejemplo, vamos a suponer que tiene que cobrar 123'45 €, una posible devolución podría ser 10 billetes de 10 euros, 1 billete de 20, 3 euros 
-y 45 céntimos. Pero estaría incorrecto, ya que estamos devolviendo muchos billetes. Lo correcto sería devolver 2 billetes de 50, un billete de 20,
-y 3 euros y 45 céntimos.
+Por ejemplo, vamos a suponer que tiene que cobrar 123'45 â‚¬, una posible devoluciÃ³n podrÃ­a ser 10 billetes de 10 euros, 1 billete de 20, 3 euros 
+y 45 cÃ©ntimos. Pero estarÃ­a incorrecto, ya que estamos devolviendo muchos billetes. Lo correcto serÃ­a devolver 2 billetes de 50, un billete de 20,
+y 3 euros y 45 cÃ©ntimos.
 
 Si se paga con tarjeta realizar las siguientes comprobaciones:
 
-Sólo debe aceptar números y espacios en blanco.
+SÃ³lo debe aceptar nÃºmeros y espacios en blanco.
 Acepta tarjetas VISA, MasterCard y American Express.
-16 dígitos Para VISA y Mastercard separados de 4 en 4 dígitos.
-Para American Express debe aceptar 15 dígitos y estar separados por 4, 6 y 5 dígitos.
-ANOTACIÓN
+16 dÃ­gitos Para VISA y Mastercard separados de 4 en 4 dÃ­gitos.
+Para American Express debe aceptar 15 dÃ­gitos y estar separados por 4, 6 y 5 dÃ­gitos.
+ANOTACIÃ“N
 
-El primer dígito indica el tipo de tarjeta:
+El primer dÃ­gito indica el tipo de tarjeta:
 - si es un 3 la tarjeta es American Express
 - si es un 4 la tarjeta es Visa
 - si es un 5 la tarjeta es MasterCard
@@ -41,21 +41,32 @@ public class Principal {
 	/* Variables */
 	// Variable para el scanner
 	static Scanner sc = new Scanner(System.in); // Variable para crear el escaner
-	// Variable para la opción selecionada
+	// Variable para la opciÃ³n selecionada
 	static String op;
 	// Variable que almacena el importe a pagar y a devolver
 	static double cuentaCliente,devolucion,efectivoCliente,cantidadADevolver;
 	// Valores de los billetes disponible
 	static int[] valoresBilletes = {50, 20, 10, 5, 1}; 
+	// Variable que almacena la cantidad a pagar por el cliente
+	static String inputPagar;
+	// Variable que almacena el nÃºmero de tarjeta
+	static String entradaTarj;
+	// Variables para indicar el tipo de tarjeta
+	static boolean americanExpress	= false;
+	static boolean visa				= false;
+	static boolean masterCard		= false;		
+	// Variable para mostrar el tipo de tarjeta
+	static String vis;
 	
 	public static void main(String[] args) {
 		menu();
 	}
 	
+	/* MÃ©todos */
 	private static void menu() {
 		System.out.println(""
-				+ "__________ Método de pago __________"
-				+ "\nSelecciona el método de pago"
+				+ "__________ MÃ©todo de pago __________"
+				+ "\nSelecciona el mÃ©todo de pago"
 				+ "\n1. Efectivo"
 				+ "\n2. Tarjeta"
 				+ "\n0. Salir");
@@ -77,7 +88,7 @@ public class Principal {
 				break;
 			}
 		} else {
-			System.err.println("Error en la selección");
+			System.err.println("Error en la selecciÃ³n");
 			menu();
 		}		
 	}
@@ -88,7 +99,7 @@ public class Principal {
 			while (true) {
 				System.out.println("Efectivo");
 	            System.out.print("Introduce la cantidad a pagar\nCantidad: ");
-	            String inputPagar = sc.nextLine();
+	            inputPagar = sc.nextLine();
 	            System.out.print("Ahora introduce el efectivo\nEfectivo: ");
 	            String inputEfectivo = sc.nextLine();
 	            try {
@@ -97,13 +108,13 @@ public class Principal {
 	                cantidadADevolver = efectivoCliente - cuentaCliente;
 	                break;
 	            } catch (NumberFormatException e) {
-	                System.err.println("No es un número válido. Inténtalo de nuevo.");
+	                System.err.println("No es un nÃºmero vÃ¡lido. IntÃ©ntalo de nuevo.");
 	            }
 	        }
 
 			 double[] valores = {500,200,100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01};
-			 	System.out.println("Devolución: " + cantidadADevolver + "\n");
-		        System.out.println("La devolución con la cantidad mínima de billetes y monedas es:\n");
+			 	System.out.println("DevoluciÃ³n: " + cantidadADevolver + "\n");
+		        System.out.println("La devoluciÃ³n con la cantidad mÃ­nima de billetes y monedas es:\n");
 
 		        for (double valor : valores) {
 		            int cantidad = (int)(cantidadADevolver / valor);
@@ -116,11 +127,102 @@ public class Principal {
 		}
 
 		private static void tarjeta() {
-			System.out.println("Tarjeta");
-//			menu();
+			System.out.println("Forma de pago mediante tarjeta seleccionado\n");
+			System.out.println("Formatos de numeraciÃ³n de las tarjetas\n"
+							 + "0000 0000 0000 0000 --> Visa\n"
+							 + "0000 0000 0000 0000 --> MasterCard\n"
+							 + "0000 000000 00000   --> American Expres\n");
+
+			System.out.print("Introduce el nÃºmero de tarjeta \nNÃºmero: ");
+			entradaTarj = sc.nextLine();
+
+			int americanOk	= entradaTarj.indexOf("3");
+			int visaOk		= entradaTarj.indexOf("4");
+			int masterOk 	= entradaTarj.indexOf("5");
+			
+			String formatoVisMasOk = "\\d{4} \\d{4} \\d{4} \\d{4}";
+			String formatoAmericanOk = "\\d{4} \\d{6} \\d{5}";
+
+
+			if (americanOk == 0) {
+				System.out.println("Tipo 	--> American Express");
+				System.out.println("NÃºmero 	--> " + entradaTarj);
+				americanExpress = true;
+				if (entradaTarj.matches(formatoVisMasOk)) {
+					System.out.println("Formato adecuado");
+					while (true) {
+			            System.out.print("Introduce la cantidad a pagar\nCantidad: ");
+			            inputPagar = sc.nextLine();
+			            try {
+			                cuentaCliente = Double.parseDouble(inputPagar);
+			                break;
+			            } catch (NumberFormatException e) {
+			                System.err.println("No es un nÃºmero vÃ¡lido. IntÃ©ntalo de nuevo.");
+			            }
+			        }
+					resumen();
+				}else {
+					System.err.println("ERROR: Formato incorrecto");
+				}
+
+			}else if (visaOk == 0) {
+				System.out.println("Tipo 	--> Visa");
+				System.out.println("NÃºmero 	--> " + entradaTarj);
+				visa = true;
+				if (entradaTarj.matches(formatoVisMasOk)) {
+					System.out.println("Formato adecuado");
+					while (true) {
+			            System.out.print("Introduce la cantidad a pagar\nCantidad: ");
+			            inputPagar = sc.nextLine();
+			            try {
+			                cuentaCliente = Double.parseDouble(inputPagar);
+			                break;
+			            } catch (NumberFormatException e) {
+			                System.err.println("No es un nÃºmero vÃ¡lido. IntÃ©ntalo de nuevo.");
+			            }
+			        }
+					resumen();
+				}else {
+					System.err.println("ERROR: Formato incorrecto");
+				}
+			}else if (masterOk == 0) {
+				System.out.println("Tipo 	--> Master Card");
+				System.out.println("NÃºmero 	--> " + entradaTarj);
+				masterCard = true;
+				if (entradaTarj.matches(formatoAmericanOk)) {
+					System.out.println("Formato adecuado");
+					while (true) {
+			            System.out.print("Introduce la cantidad a pagar\nCantidad: ");
+			            inputPagar = sc.nextLine();
+			            try {
+			                cuentaCliente = Double.parseDouble(inputPagar);
+			                break;
+			            } catch (NumberFormatException e) {
+			                System.err.println("No es un nÃºmero vÃ¡lido. IntÃ©ntalo de nuevo.");
+			            }
+			        }
+					resumen();
+				}else {
+					System.err.println("ERROR: Formato incorrecto");
+				}
+			}
+
+
 		}
-
-	
-	
-
+		private static void resumen() {
+			if (visa) {
+				vis = "Visa";
+			}else if (americanExpress) {
+				vis = "American Express";
+			}else if (masterCard) {
+				vis = "MasterCard";
+			}
+			System.out.println("\n-------------------------------------------------");
+			System.out.println("Resumen de la operaciÃ³n");
+			System.out.println("Tipo --> " + vis);
+			System.out.println("Cantidad a abonar: " + inputPagar + " â‚¬");
+			System.out.println("NÃºmero de tarjeta: " + entradaTarj + "");
+			System.out.println("Estado --> PAGO REALIZADO");
+			System.out.println("-------------------------------------------------");
+		}
 }
